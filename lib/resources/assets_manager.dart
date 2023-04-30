05b0b3f8187fdede2de791a1f04e0c75b2158137
Komai25 <101812642+Komai25@users.noreply.github.com>
2023-04-30 16:52:14 +0300

    Update assets_manager.dart

--- lib/resources/assets_manager.dart
+++ lib/resources/assets_manager.dart
@@ -2,7 +2,8 @@ const String imagePath = 'assets/images';
 const String jsonPath = "assets/json";
 
 class ImagesAssets {
-  static const String splashLogo = '$imagePath/WhatsApp-Image-2023-04-28-at-13.35.55.sv';
+  static const String splashLogo =
+      '$imagePath/WhatsApp-Image-2023-04-28-at-13.35.55.svg';
 }
 
 class JsonAssets {
